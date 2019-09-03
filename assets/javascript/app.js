fa235156477c4c0e0098cbbf77ad0ef20b6509d1
Emerson Doyah <emerson.doyah@gmail.com>
2019-09-03 16:44:50 -0500

    refactor

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,42 @@
 $(document).ready(function () {
-  if (browserIsSupported()) {
-    $('#bg-home').addClass('polygon');
+
+  addPolygonStyle();
+  addInterneExplorerStyles();
+
+  function addPolygonStyle() {
+    if (browserIsSupported()) {
+      $('#bg-home').addClass('polygon');
+    }
   }
 
-  if (browerIsMicrosoft()) {
-    addIESpecificClasses();
+  function browserIsSupported() {
+    const supportedBrowsers = ['Chrome', 'Safari', 'Firefox'];
+    return checkAnyExist(supportedBrowsers);
+  }
+
+  function checkAnyExist(browsers) {
+    const agent = getUserAgent();
+    return browsers.some(browser => agent.includes(browser));
+  }
+
+  function getUserAgent() {
+    return window.navigator.userAgent;
+  }
+
+  function addInterneExplorerStyles() {
+    if (browerIsMicrosoft()) {
+      addIESpecificClasses();
+    }
+  }
+
+  function browerIsMicrosoft() {
+    const microsoftBrowsers = ['MSIE', 'Edge'];
+    return checkAnyExist(microsoftBrowsers);
+  }
+
+  function addIESpecificClasses() {
+    $('.logo, .skill-heading').addClass('IE-background');
+    $('.skillsGroup').addClass('IE-border-left');
   }
 
   $('body').scrollspy({
@@ -13,10 +45,20 @@ $(document).ready(function () {
   });
 
   $(".nav-link").on('click', function (event) {
-    if (this.hash) {
-      scrollToPosition(event);
+    const hash = this.hash;
+    if (hash) {
+      scrollToPosition(event, hash);
     }
   });
+
+  function scrollToPosition(event, hash) {
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 1000, function () {
+      window.location.hash = hash;
+    });
+  }
 
   $(window).scroll(function () {
     if (pagePassedNavbarPosition()) {
@@ -26,9 +68,71 @@ $(document).ready(function () {
     }
   });
 
+  function pagePassedNavbarPosition() {
+    const pageDistanceFromTop = 500;
+    const pageTop = getDocumentScrollTop();
+    return pageTop > pageDistanceFromTop;
+  }
+
+  function getDocumentScrollTop() {
+    return $(document).scrollTop();
+  }
+
+  function makeBackgroundDark() {
+    $('.navbar').addClass('bg-dark');
+    $('.fixed-top').css('padding-top', '0.5rem');
+  }
+
+  function checkToggleButton() {
+    if (!toggleButtonIsExpended()) {
+      makeBackgroundLight();
+    }
+  }
+
+  function toggleButtonIsExpended() {
+    return $('#toggle').attr('aria-expanded') === 'true';
+  }
+
+  function makeBackgroundLight() {
+    $('.navbar').removeClass('bg-dark');
+    $('.fixed-top').css('padding-top', '3rem');
+  }
+
   $(document).scroll(function () {
     animatePageSection();
   });
+
+  function animatePageSection() {
+    const mapping = getPagePositionMapping();
+    $.each(mapping, function (section, position) {
+      const $element = $(section);
+      animate($element, position);
+    });
+  }
+
+  function getPagePositionMapping() {
+    const section = {
+      about: '.aboutMe',
+      social: '.social-div',
+      skill: '.mySkills',
+      projects: '.myProjects',
+      contact: '.myContact',
+    };
+
+    return {
+      [section.about]: 360,
+      [section.social]: 720,
+      [section.skill]: 1250,
+      [section.projects]: 1575,
+      [section.contact]: 5888,
+    };
+  }
+
+  function animate(element, pagePosition) {
+    if (pageIsAtCurrentPosition(pagePosition)) {
+      element.addClass('show-element');
+    }
+  }
 
   $('#toggle').click(function () {
     if (toggleButtonIsExpended()) {
@@ -41,115 +145,24 @@ $(document).ready(function () {
   $(document).on('scroll', function () {
     animateProjectImages();
   })
+
+  function animateProjectImages() {
+    $('.project-div').each(function () {
+      if (scrollingIsAtCurrentImage($(this))) {
+        $(this).find('.image-card').css('margin', '0rem');
+      }
+    });
+  }
+
+  function scrollingIsAtCurrentImage(projectElement) {
+    const pageTop = getDocumentScrollTop();
+    const currentPositionFromTop = projectElement.position().top
+    const offset = 650;
+    return (pageTop >= (currentPositionFromTop - offset));
+  }
+
+  function pageIsAtCurrentPosition(pagePosition) {
+    const pageTop = getDocumentScrollTop();
+    return pageTop > pagePosition;
+  }
 });
-
-function browserIsSupported() {
-  const agent = getUserAgent();
-  return (agent.includes("Chrome") || agent.includes("Safari") || agent.includes("Firefox"));
-}
-
-function getUserAgent() {
-  return window.navigator.userAgent;
- }
-
- function browerIsMicrosoft() {
-  const agent = getUserAgent();
-  return agent.includes("MSIE") || agent.includes('Edge');
-}
-
-function addIESpecificClasses() {
-  $('.logo, .skill-heading').addClass('IE-background');
-  $('.skillsGroup').addClass('IE-border-left');
-}
-
-function scrollToPosition(event) {
-  event.preventDefault();
-  const hash = this.hash;
-  $('html, body').animate({
-    scrollTop: $(hash).offset().top
-  }, 1000, function () {
-    window.location.hash = hash;
-  });
-}
-
-function pagePassedNavbarPosition() {
-  const pageDistanceFromTop = 500;
-  return $(document).scrollTop() > pageDistanceFromTop;
-}
-
-function makeBackgroundDark() {
-  $('.navbar').addClass('bg-dark');
-  $('.fixed-top').css('padding-top', '0.5rem');
-}
-
-function checkToggleButton() {
-  if (!toggleButtonIsExpended()) {
-    makeBackgroundLight();
-  }
-}
-
-function toggleButtonIsExpended() {
-  return $('#toggle').attr('aria-expanded') === 'true';
-}
-
-function makeBackgroundLight() {
-  $('.navbar').removeClass('bg-dark');
-  $('.fixed-top').css('padding-top', '3rem');
-}
-
-function animatePageSection() {
-  const mapping = getPagePositionMapping();
-  $.each(mapping, function(section, position){
-      const $element =  $(section);
-      animate($element, position);
-  });
-}
-
-function getPagePositionMapping() { 
-  const section = {
-    about:   '.aboutMe',
-    social: '.social-div',
-    skill: '.mySkills',
-    projects: '.myProjects',
-    contact: '.myContact',
-  };
-
- return {
-    [section.about]: 360,
-    [section.social]: 720,
-    [section.skill]: 1250,
-    [section.projects]: 1575,
-    [section.contact]: 5888,
-  };
- }
-
- function animate(element, pagePosition) {
-  if (pageIsAtCurrentPosition(pagePosition)) {
-    element.addClass('show-element');
-  }
-}
-
-function animateProjectImages() {
-  $('.project-div').each(function () {
-    if (scrollingIsAtCurrentImage($(this))) {
-      $(this).find('.image-card').css('margin', '0rem');
-    }
-  });
-}
-
-function scrollingIsAtCurrentImage(projectElement) {
-  const pageTop = getDocumentScrollTop();
-  const currentPositionFromTop = projectElement.position().top
-  const offset = 650;
-  return (pageTop >= (currentPositionFromTop - offset));
-}
-
-function getDocumentScrollTop() {
-  return $(document).scrollTop();
-}
-
-function pageIsAtCurrentPosition(pagePosition) {
-  const pageTop = getDocumentScrollTop();
-  return pageTop > pagePosition;
-}
-
